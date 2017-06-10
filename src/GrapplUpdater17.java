3899ab5d1a580114b9b08e270de4a62bd31e1827
Ellen Hebert <ellenhebert@icloud.com>
2017-06-09 22:38:15 -0400

    Add GrapplUpdater17 class

---
+++ src/GrapplUpdater17.java
@@ -0,0 +1,12 @@
+public class GrapplUpdater17 {
+
+    public static void main(String[] args) {
+        // Check website to retrieve MD5 code
+        /**
+         * Check to see if .jar already exists on disk
+         * If it does, get the MD5 hash
+         * If it doesn't, download the .jar
+         */
+
+    }
+}
